e6e827b5450a2a2ad7ac978dc64b1fc617e64465
Marble178NewAc <97432282+Marble178NewAc@users.noreply.github.com>
2022-01-23 15:03:34 +0000

    The answer is chonk now
    Dont need to worry, the random will be imported even if your not typing "import random"

--- 8ball.py
+++ 8ball.py
@@ -1,4 +1,4 @@
 Question = input("Ask a question:\n")
-Answerlist = ["Yes", "No"]
+Answerlist = ["Yes", "No", "Yeah", "Nope", "Most likely", "You may rely on it...", "Don't count on it..."]
 Answer = __import__("random").choice(Answerlist)
 print(f"Question: {Question}\nAnswer: {Answer}") 
